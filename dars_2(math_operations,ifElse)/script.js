fa1c98f29c103054232ffbf90802d1eec8517df7
Diyor <baxacap@gmail.com>
2025-06-20 14:37:53 +0500

    matematik operatorlar, taqqoslash operatorlari va shart operatorlari

---
+++ dars_2(math_operations,ifElse)/script.js
@@ -0,0 +1,41 @@
+// Matematik operatorlar
+let a = 30
+let b = 2
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// Taqqoslash operatorlari
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+console.log(5 = 5); // true
+console.log(5 === "5"); // false ma'lumot turini ham tekshiradi
+console.log(5 != 4); // true ! -> emas ma'nosini bildiradi
+
+
+// vazifa
+let yosh = 18
+
+if (yosh >= 18){
+    console.log("Siz voyaga yetgansiz");
+}else{
+    console.log("Siz hali voyaga yetmagansiz");
+    
+}
+
+// vazifa 2
+let baho = 3
+
+if (baho === 5){
+    console.log("A'lo");
+} else if (baho === 4){
+    console.log("Yaxshi");
+} else if (baho === 3){
+    console.log("Qoniqarli");
+} else{
+    console.log("Qoniqarsiz");
+    
+}

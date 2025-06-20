2e2b8ed8fdc0a003ad6c8c4a6b03f02013346bf7
Diyor <baxacap@gmail.com>
2025-06-20 14:26:28 +0500

    firs commit

---
+++ dars_1/script.js
@@ -0,0 +1,26 @@
+// console log
+console.log(15)
+console.log("Hello World")
+
+// O'zgaruvchilar nomlash turlari: let var const
+let ism = "Diyor" // buni qiymatini o'zgartirsa bo'ladi va bu yangi variant (default)
+const raqam = 7 // buni qiymatini o'zgartirsa bo'maydi bu ham yangi default variant (default)
+var haqiqat = true  // buni qiymatini o'zgartirsa bo'adi va bunday qo'llash usuli maslahat berilmaydi
+
+// Ma'lumot turlari
+let son = 5 // integer
+let js = "JavaScript" // string
+let haq = true || false // boolean
+let nol = null // Aniq qiymat yo'q
+let bironNima;  //undefined
+
+// Vazifa
+let name = "Diyor"
+let yosh = 16
+let shahar = "Samarqand"
+let itfanmi = true
+
+console.log(name, typeof ism)
+console.log(yosh, typeof yosh)
+console.log(shahar, typeof shahar)
+console.log(itfanmi, typeof itfanmi)

20d67c88738774ff6a89809ba64728a39cad8e39
Diyor <baxacap@gmail.com>
2025-06-20 14:47:58 +0500

    alert prompt va confirm

--- dars_3(alert,prompt,confirm)/script.js
+++ dars_3(alert,prompt,confirm)/script.js
@@ -1,8 +1,8 @@
 // alert modal built in
 alert("Salom");
 // prompt - brauzer js inputi
-let ism = prompt("Ismingizni kiriting");
-alert(ism);
+let isim = prompt("Ismingizni kiriting");
+alert(isim);
 
 // confirm
 let tasdiq = confirm("Haqiqatan ham davom etasizmi?");
@@ -10,4 +10,17 @@ if (tasdiq) {
   alert("Siz davom etdingiz!");
 } else {
   alert("Siz bekor qildingiz!");
+}
+
+// vazifa
+let ism = prompt("Iltimos, ismingizni kiriting:");
+let yosh = prompt("Iltimos, yoshingizni kiriting:");
+
+alert(`Salom, ${ism}, siz ${yosh} yoshdasiz`);
+
+let davommi = confirm("Saytga kirishni davom ettirasizmi?");
+if (davommi) {
+  alert(`Xush kelibsiz, ${ism}!`);
+} else {
+  alert(`Xayr, ${ism}.`);
 }

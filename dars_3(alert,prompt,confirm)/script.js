b03bb8d57439f4b8bdac41cd5b670c5c010a09ed
Diyor <baxacap@gmail.com>
2025-06-20 14:43:14 +0500

    alert promp va confirm

---
+++ dars_3(alert,prompt,confirm)/script.js
@@ -0,0 +1,13 @@
+// alert modal built in
+alert("Salom");
+// prompt - brauzer js inputi
+let ism = prompt("Ismingizni kiriting");
+alert(ism);
+
+// confirm
+let tasdiq = confirm("Haqiqatan ham davom etasizmi?");
+if (tasdiq) {
+  alert("Siz davom etdingiz!");
+} else {
+  alert("Siz bekor qildingiz!");
+}
